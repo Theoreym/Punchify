dadd2a08deaae81ae37f805e8e69e8a1ef59e2ed
Theoreym <mtstheo@gmail.com>
2024-03-19 11:06:53 +0100

    t7

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -5,6 +5,20 @@ const Injury = require('../models/injuryModel');
 const User = require('../models/userModel');
 const Convoke = require("../models/convokeModel");
 
+// Middleware
+const authenticate = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.session.userId);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   get: async (req, res) => {
