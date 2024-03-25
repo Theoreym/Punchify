dfee026aca493099aab203d50b62ba72b837a50e
hehodav <hehodav@gmail.com>
2024-03-25 13:41:48 +0100

    Corection teamController for team_manage.hbs

--- api/controllers/teamController.js
+++ api/controllers/teamController.js
@@ -4,6 +4,7 @@ const { validationResult } = require('express-validator');
 
 module.exports = {
     getList: async (req, res) => {
+        const navTeamManage = true;
         const team = await Team.findAll({ raw: true });
         //console.log(team);
         res.render('team_manage', {team, navTeamManage});
